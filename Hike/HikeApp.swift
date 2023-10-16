46d78a30dbe3fdab7082cf412ae744d13d0cd5ff
coburndaniel979 <coburndaniel979@gmail.com>
2023-10-16 18:12:24 -0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Daniel Coburn on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
